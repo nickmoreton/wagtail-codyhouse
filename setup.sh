cd4c5fb73bbe2a0b6e66cb2f55cd7fbdf2061bce
Nick Moreton <nickmoreton@me.com>
2020-07-18 19:13:39 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,10 +8,11 @@ rm master.zip && rm LICENSE.md && rm README.md && rm gulpfile.js &&
 npm install -g npm && npm install && 
 npm install -g gulp &&
 pyenv local 3.5-dev &&
-python -m venv venv && source venv/bin/activate &&
-pip install wagtail && wagtail start config . &&
-rm Dockerfile && rm requirements.txt &&
-pip install pip --upgrade && mkdir requirements && pip freeze > requirements/base.txt
-python manage.py migrate && python manage.py collectstatic <<<yes &&
-python manage.py runserver 0.0.0.0:8000 && 
-npm run gulp watch
+python -m venv venv
+#&& source venv/bin/activate &&
+#pip install wagtail && wagtail start config . &&
+#rm Dockerfile && rm requirements.txt &&
+#pip install pip --upgrade && mkdir requirements && pip freeze > requirements/base.txt
+#python manage.py migrate && python manage.py collectstatic <<<yes &&
+#python manage.py runserver 0.0.0.0:8000 && 
+#npm run gulp watch
