eaf4e4e256bf589a9a7f764a4f9a4b36f77474a4
Nick Moreton <nickmoreton@me.com>
2020-07-19 21:45:56 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,18 +1,8 @@
 #!/bin/bash
 
-wget https://github.com/CodyHouse/codyhouse-framework/archive/master.zip &&
-unzip master.zip &&
-mv codyhouse-framework-master/** . &&
-rm -R codyhouse-framework-master && 
-rm master.zip && rm LICENSE.md && rm README.md && rm gulpfile.js &&
-npm install -g npm && npm install && 
-npm install -g gulp &&
-pyenv local 3.5-dev &&
-python -m venv venv
-#&& source venv/bin/activate &&
-#pip install wagtail && wagtail start config . &&
-#rm Dockerfile && rm requirements.txt &&
-#pip install pip --upgrade && mkdir requirements && pip freeze > requirements/base.txt
-#python manage.py migrate && python manage.py collectstatic <<<yes &&
-#python manage.py runserver 0.0.0.0:8000 && 
-#npm run gulp watch
+pip install wagtail \
+&& wagtail start config . \
+&& rm Dockerfile \
+&& rm requirements.txt \
+&& chmod +x get_frontend.sh \
+&& rm get_frontend.sh
