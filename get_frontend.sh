77b32e06f47515318fe51abd679d3057407ac0f6
Nick Moreton <nickmoreton@me.com>
2020-07-19 21:11:34 +0100

    Create get_frontend.sh

---
+++ get_frontend.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# gets codyhouse-framework and extracts to _theme
+wget https://github.com/CodyHouse/codyhouse-framework/archive/master.zip && unzip master.zip \
+&& mv codyhouse-framework-master/** . && mv main _theme \
+&& rm -R codyhouse-framework-master && rm master.zip && rm LICENSE.md && rm README.md && rm gulpfile.js
