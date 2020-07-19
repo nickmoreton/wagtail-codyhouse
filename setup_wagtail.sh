5c6ff80dcdb05fcd4ea6b388d116d3e43012c6f5
Nick Moreton <nickmoreton@me.com>
2020-07-19 22:07:24 +0100

    Update setup_wagtail.sh

--- setup_wagtail.sh
+++ setup_wagtail.sh
@@ -2,6 +2,8 @@
 
 pip install wagtail \
 && wagtail start config . \
+&& ./mananage.py migrate \
+&& ./manange.py collectstatic \
 && rm Dockerfile \
 && rm requirements.txt \
 && chmod +x get_frontend.sh \
